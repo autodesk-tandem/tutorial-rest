a198734ff053288c79222bb17dcc8e00cd9d8fe8
Jan Liska <jan.liska@autodesk.com>
2023-10-12 10:49:00 +0200

    added example to assign classification

---
+++ classification/assign-element-classification.js
@@ -0,0 +1,97 @@
+/*
+    This example demonstrates how assign classification to the element. The scenario is quite simple - there is hardcoded mapping between element name
+    and classification. I can be used as reference sample in case that more advanced logic is needed.
+    
+    It uses 2-legged authentication - this requires that application is added to facility as service.
+*/
+import { createToken } from '../common/auth.js';
+import { TandemClient } from '../common/tandemClient.js';
+import { ColumnFamilies, ColumnNames, MutateActions, QC } from '../common/utils.js';
+
+// update values below according to your environment
+const APS_CLIENT_ID = 'YOUR_CLIENT_ID';
+const APS_CLIENT_SECRET = 'YOUR_CLIENT_SECRET';
+const FACILITY_URN = 'YOUR_FACILITY_URN';
+
+async function main() {
+    // STEP 1 - obtain token to authenticate subsequent API calls
+    const token = await createToken(APS_CLIENT_ID,
+        APS_CLIENT_SECRET, 'data:read data:write');
+    const client = new TandemClient(() => {
+        return token;
+    });
+
+    // STEP 2 - get facility & facility template
+    const facilityId = FACILITY_URN;
+    const facility = await client.getFacility(facilityId);
+    const template = await client.getFacilityTemplate(facilityId);
+    // this defines mapping between element name and classification
+    const elementClassificationMap = {
+        'Door - Interior - Double' : 'Interior Doors',
+        'Door - Interior - Single' : 'Interior Doors',
+        'Door - Exterior - Double' : 'Exterior Doors',
+        'Door - Exterior - Single' : 'Exterior Doors'
+    };
+    // STEP 3 - build map between element name and classification id
+    const classificationMap = {};
+
+    for (const name in elementClassificationMap) {
+        const classificationName = elementClassificationMap[name];
+        const classificationData = template.classification.rows.find(r => {
+            return r[1] === classificationName;
+        });
+
+        if (classificationData) {
+            classificationMap[name] = classificationData[0];
+        }
+    }
+    // STEP 4 - iterate through facility models and apply classification based on mapping
+    for (const link of facility.links) {
+        const elements = await client.getElements(link.modelId);
+        const keys = [];
+        const mutations = [];
+
+        for (const element of elements) {
+            const elementName = element[QC.Name];
+
+            if (!elementName) {
+                continue;
+            }
+            const classificationId = classificationMap[elementName];
+
+            if (!classificationId) {
+                continue;
+            }
+            // we don't want to apply same classification again
+            if ((element[QC.Classification] === classificationId) || (element[QC.OClassification] === classificationId)) {
+                continue;
+            }
+            console.log(`${elementName}:${element[QC.Key]}`);
+            // store keys and mutations
+            keys.push(element[QC.Key]);
+            mutations.push([
+                MutateActions.Insert,
+                ColumnFamilies.Standard,
+                ColumnNames.OClassification,
+                classificationId
+            ]);
+        }
+        if (keys.length === 0) {
+            continue;
+        }
+        // STEP 5 - apply changes
+        await client.mutateElements(link.modelId, keys, mutations, 'Update classification');
+
+        console.log(`Updated elements: ${keys.length}`);
+    }
+}
+
+main()
+    .then(() => {
+        console.log('success');
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error('failure', err);
+        process.exit(1);
+    });
