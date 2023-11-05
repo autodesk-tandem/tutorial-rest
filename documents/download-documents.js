b8a4d044983df6a267103e5394e24012da4abe1c
Jan Liska <jan.liska@autodesk.com>
2023-11-05 20:14:09 +0100

    added example to download linked documents

---
+++ documents/download-documents.js
@@ -0,0 +1,44 @@
+/*
+    This example demonstrates how to download linked documents from given facility.
+    
+    It uses 2-legged authentication - this requires that application is added to facility as service.
+*/
+import { createToken } from '../common/auth.js';
+import { TandemClient } from '../common/tandemClient.js';
+
+// update values below according to your environment
+const APS_CLIENT_ID = 'YOUR_CLIENT_ID';
+const APS_CLIENT_SECRET = 'YOUR_CLIENT_SECRET';
+const FACILITY_URN = 'YOUR_FACILITY_URN';
+const DOWNLOAD_PATH = 'C:/Temp';
+
+async function main() {
+    // STEP 1 - obtain token. The sample uses 2-legged token but it would also work with 3-legged token
+    // assuming that user has access to the facility
+    const token = await createToken(APS_CLIENT_ID,
+        APS_CLIENT_SECRET, 'data:read');
+    const client = new TandemClient(() => {
+        return token;
+    });
+    
+    // STEP 2 - get facility
+    const facilityId = FACILITY_URN;
+    const facility = await client.getFacility(facilityId);
+
+    // STEP 3 - iterate through linked documents
+    for (const doc of facility.docs) {
+        console.log(`${doc.name}: ${doc.id}`);
+        // STEP 4 - download document to local file
+        await client.saveDocumentContent(doc.signedLink, `${DOWNLOAD_PATH}/${doc.name}`);
+    }
+}
+
+main()
+    .then(() => {
+        console.log('success');
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error('failure', err);
+        process.exit(1);
+    });
