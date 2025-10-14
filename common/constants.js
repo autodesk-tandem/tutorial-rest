218cb6e193f52d80b8638212aa0a6eb3cf4ef057
Jan Liska <jan.liska@autodesk.com>
2025-10-14 08:34:35 +0200

    move system class names to constants

--- common/constants.js
+++ common/constants.js
@@ -134,3 +134,32 @@ export const AttributeType = {
     Url: 25,
     StringList: 40
 };
+
+export const SystemClassNames = [
+	"Supply Air",                   //0
+	"Return Air",                   //1
+	"Exhaust Air",                  //2
+	"Hydronic Supply",              //3
+	"Hydronic Return",              //4
+	"Domestic Hot Water",           //5
+	"Domestic Cold Water",          //6
+	"Sanitary",                     //7
+	"Power",                        //8
+	"Vent",                         //9
+	"Controls",                     //10
+	"Fire Protection Wet",          //11
+	"Fire Protection Dry",          //12
+	"Fire Protection Pre-Action",   //13
+	"Other Air",                    //14
+	"Other",                        //15
+	"Fire Protection Other",        //16
+	"Communication",                //17
+	"Data Circuit",                 //18
+	"Telephone",                    //19
+	"Security",                     //20
+	"Fire Alarm",                   //21
+	"Nurse Call",                   //22
+	"Switch Topology",              //23
+	"Cable Tray Conduit",           //24
+	"Storm",                        //25
+];
--- common/utils.js
+++ common/utils.js
@@ -9,38 +9,10 @@ import {
     kRecordSize,
     kSystemIdSize,
     ElementFlags,
-    KeyFlags
+    KeyFlags,
+    SystemClassNames
 } from './constants.js';
 
-const SystemClassNames = [
-	"Supply Air",                   //0
-	"Return Air",                   //1
-	"Exhaust Air",                  //2
-	"Hydronic Supply",              //3
-	"Hydronic Return",              //4
-	"Domestic Hot Water",           //5
-	"Domestic Cold Water",          //6
-	"Sanitary",                     //7
-	"Power",                        //8
-	"Vent",                         //9
-	"Controls",                     //10
-	"Fire Protection Wet",          //11
-	"Fire Protection Dry",          //12
-	"Fire Protection Pre-Action",   //13
-	"Other Air",                    //14
-	"Other",                        //15
-	"Fire Protection Other",        //16
-	"Communication",                //17
-	"Data Circuit",                 //18
-	"Telephone",                    //19
-	"Security",                     //20
-	"Fire Alarm",                   //21
-	"Nurse Call",                   //22
-	"Switch Topology",              //23
-	"Cable Tray Conduit",           //24
-	"Storm",                        //25
-];
- 
 export class Encoding {
     /**
      * Decodes base64 encoded string.
