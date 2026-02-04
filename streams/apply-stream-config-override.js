69e2ba14e05b21237e185c6041dd836d70c1ee17
Jan Liska <jan.liska@autodesk.com>
2026-02-04 15:17:19 +0100

    removed unused import

--- streams/apply-stream-config-override.js
+++ streams/apply-stream-config-override.js
@@ -6,7 +6,7 @@
 import { createToken } from '../common/auth.js';
 import { TandemClient } from '../common/tandemClient.js';
 import { QC } from '../common/constants.js';
-import { Encoding, getDefaultModel } from '../common/utils.js';
+import { getDefaultModel } from '../common/utils.js';
 
 // update values below according to your environment
 const APS_CLIENT_ID = 'YOUR_CLIENT_ID';
